f416ca357f70772f1eb413d637968624b612a3c8
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2023-09-27 07:34:52 -0300

    source error

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "https://lkcards-api.onrender.com/*",
+        source: "/chat/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
